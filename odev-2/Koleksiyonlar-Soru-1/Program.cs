d703d7b85fb7d6ba754168d58901d5ac6b854e13
romrem7 <127850740+romrem7@users.noreply.github.com>
2023-10-27 16:56:10 +0300

    Add files via upload

---
+++ odev-2/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,109 @@
+﻿// Soru - 1: Klavyeden girilen 20 adet pozitif sayının asal ve asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+
+        // Negatif ve numeric olmayan girişleri engelleyin.
+        // Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+        // Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+
+        ﻿using System;
+using System.Collections;
+
+namespace degisken{
+
+    class Program{
+        static void Main(string[] args){
+            ArrayList asal = new ArrayList(); 
+            ArrayList asalOlmayan = new ArrayList();
+            ArrayList sayilar = new ArrayList();
+            int asalSayaci=0, asalOlmayanSayaci=0;
+            int asalOrtalama=0, asalOlmayanOrtalama=0;
+            int asalToplam=0, asalOlmayanToplam=0;
+            
+            Console.WriteLine("Sayilari giriniz");
+            for(int i=0 ; i<20 ; i++){ 
+                try{
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    
+                    if(n <= 0){
+                        Console.WriteLine("Pozitif tam sayı giriniz.");
+                    }
+
+                    else{
+                        sayilar.Add(n);
+                    }
+                }
+                catch (FormatException )
+                {
+                    Console.WriteLine("Veri tipi uygun değil. Girilen deger bir sayi olmalidir.");    
+                }
+            }    
+                
+                
+                
+            foreach (var item in sayilar){
+                if(asalKontrol(Convert.ToInt32(item))==true){
+                    asal.Add(item);
+                    asalSayaci++;
+                    asalToplam=asalToplam + Convert.ToInt32(item);
+                }
+                else{
+                    asalOlmayan.Add(item);
+                    asalOlmayanSayaci++;
+                    asalOlmayanToplam=asalOlmayanToplam + Convert.ToInt32(item);
+                }
+            }
+
+            asalOrtalama = asalToplam/asalSayaci;
+            asalOlmayanOrtalama = asalOlmayanToplam/asalOlmayanSayaci;
+                
+            asal.Sort();
+            asal.Reverse();
+            asalOlmayan.Sort();
+            asalOlmayan.Reverse();
+            
+
+            Console.Write("Asal Sayilarin büyükten kücüge dogru siralanmasi: "); 
+            foreach (var item in asal)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Asal Sayilar dizisinin eleman sayisi:" + asalSayaci);
+            Console.WriteLine("Asal Sayilar dizisinin ortalamasi:" + asalOrtalama);
+
+            Console.Write("Asal Olmayan Sayilarin büyükten kücüge dogru siralanmasi: ");
+            foreach (var item in asalOlmayan)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Asal Olmayan Sayilar dizisinin eleman sayisi:" + asalOlmayanSayaci);
+            Console.WriteLine("Asal Sayilar dizisinin ortalamasi:" + asalOlmayanOrtalama);
+            
+        }
+        public static bool asalKontrol(int sayi){
+            int sayac = 0;
+            bool asal;
+            int i;
+            
+            for(i = 1; i <= sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    sayac++;
+                }
+            }
+
+            if (sayac == 2)
+            {
+                asal = true;
+            }
+
+            else
+            {
+                asal = false;
+            }
+
+            return asal;
+        } 
+    }
+}
---
+++ odev-2/Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,102 @@
+﻿﻿using System;
+using System.Collections;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru - 2: Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve ortalama toplamlarını console'a yazdıran programı yazınız. (Array sınıfını kullanarak yazınız.)
+
+            Console.WriteLine("20 Adet Sayı Giriniz");
+            ArrayList alignment = new ArrayList();
+            ArrayList bigNum = new ArrayList();
+            ArrayList littleNum = new ArrayList();
+            
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine((i+1) + ". Sayıyı Giriniz");
+                int number =Convert.ToInt32(Console.ReadLine());
+                
+                bigNum.Add(number);
+                littleNum.Add(number);
+                alignment.Add(number);
+
+            }
+            Console.WriteLine("BÜYÜKTEN KÜÇÜĞE İLK 3 SAYI");
+
+            bigNum.Sort();
+            bigNum.Reverse();
+            
+            for (int i = 0; i < 3; i++)
+            {   
+                Console.Write(bigNum[i]);
+            }
+            bigNum.RemoveRange(3,17);
+
+            //ArrayList Dizi'ye Çevirilip Dizi Metodu Kullanıldı
+            int[] arrBigNum = new int[3];
+            arrBigNum =(int[]) bigNum.ToArray(typeof(int));
+            
+            int bigNumTotal =Sum(arrBigNum);
+            double bigNumAverage = Average(arrBigNum);
+
+            Console.WriteLine("\n Toplam = "+ bigNumTotal);
+            Console.WriteLine("Ortalama = " + bigNumAverage );
+            
+            Console.WriteLine("**************************");
+
+            
+            Console.WriteLine("\n KÜÇÜKTEN BÜYÜĞE İLK 3 SAYI");
+            
+            littleNum.Sort();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(littleNum[i]);
+            }
+            littleNum.RemoveRange(3,17);
+
+            //ArrayList Dizi'ye Çevirilip Dizi Metodu Kullanıldı
+            int[] arrLittleNum = new int[3];
+            arrLittleNum =(int[]) littleNum.ToArray(typeof(int));
+            
+            int littleNumTotal = Sum(arrLittleNum);
+            double littleNumAverage = Average(arrLittleNum);
+
+            Console.WriteLine("\n Toplam = "+ littleNumTotal);
+            Console.WriteLine("Ortalama = " + littleNumAverage);
+
+            Console.WriteLine("ORTALAMALARIN TOPLAMI");
+
+            double totalAverage = littleNumAverage + bigNumAverage;
+            Console.WriteLine(totalAverage);
+        }
+        //Toplam Metodu
+        static int Sum(int[] num)
+        {
+            int total =  0;
+           
+            for (int i = 0; i < num.Length; i++)
+            {
+                total += num[i] ;
+            }
+            return total;
+        }
+        //Ortalama Metodu
+        static double Average(int[] num)
+        {
+            double average = 0;
+            int total = 0;
+
+            for (int i = 0; i < num.Length; ++i)
+            {
+                total += num[i]; 
+            }
+            average = (double)total / (num.Length);
+
+            return average;
+        }
+    }
+}
